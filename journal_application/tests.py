915993f9d0007e8c08dd219b56e119b555d9fde5
darionbadillo <dbadillo@uccs.edu>
2023-11-08 09:12:34 -0700

    Begin Tests.py

--- journal_application/tests.py
+++ journal_application/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
-
+from django.test import TestCase, LiveServerTestCase
+from selenium import webdriver
 # Create your tests here.
+
+class Notebook(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_can_create_notebook(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Notebook', self.browser.title)
+        self.fail('Finish the test!')
